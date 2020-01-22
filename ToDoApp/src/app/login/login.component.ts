f6d64af97048d9e0119f77e936d99ee97226259a
ShannonMolina <shannonmolina77@yahoo.com>
2020-01-21 23:07:37 -0600

    Update login.component.ts

--- ToDoApp/src/app/login/login.component.ts
+++ ToDoApp/src/app/login/login.component.ts
@@ -1,51 +1,25 @@
-import { Component, OnInit, createPlatformFactory } from '@angular/core';
-import { User } from '../user';
-import { UserserviceService } from '../userservice.service';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from  '@angular/forms'; 
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
-})
-export class LoginComponent implements OnInit {
+import { LoginComponent } from './login.component';
 
-  returnedId;
-  userId;
-  form : FormGroup;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
-  constructor(private userService : UserserviceService, private fb: FormBuilder, private router: Router) { }
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LoginComponent ]
+    })
+    .compileComponents();
+  }));
 
-  ngOnInit() {
-    this.createForm();
-  }
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-  createForm(){
-    this.form = this.fb.group({
-      userName: ['', Validators.required], 
-      userPassword: ['', Validators.required]
-    });
-  }
-
-  validateUser(id, password){
-      this.returnedId = this.userService.getUserById(id);
-      console.log(this.returnedId);
-      if(this.returnedId == -1){
-        console.log(this.returnedId);
-        //user is not valid
-      }else{
-        this.userId = this.returnedId;
-        console.log(this.userId);
-      }
-  }
-
-  onSubmit(userId, userPassword){
-     this.validateUser(userId, userPassword);
-    // this.returnedId = this.userService.getUserById(userId);
-    // console.log(this.returnedId);
-    
-  }
-
-}
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
